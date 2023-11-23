ee0d2ded31ad529f13b0beb031cd516d65d2d9aa
kira <arooji25@live.com>
2023-11-23 14:58:37 +0300

    a

---
+++ W7-D5-ProjectTuaiq/src/component/Reg.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Reg() {
+  return (
+    <div className='lastSec'>
+        <h1>خطوات التسجيل</h1>
+        <div className='reg'>
+           
+           <div  className='regSteps'>
+            <img src='https://tuwaiq.edu.sa/img/course-step-1.svg'></img>
+             <h3>الانضمام للمنصـــة</h3>
+           </div>
+           <div  className='regSteps'>
+            <img src='https://tuwaiq.edu.sa/img/course-step-2.svg'></img>
+             <h3>التــســــــــجيــــــــــــل في المعسكر أو البرنامج</h3>
+           </div>
+           <div  className='regSteps'>
+            <img src='https://tuwaiq.edu.sa/img/course-step-3.svg'></img>
+             <h3> انتظار رسالة القــــبــــــــــــول</h3>
+           </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Reg
