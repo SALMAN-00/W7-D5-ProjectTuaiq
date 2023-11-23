120539142c3272a1d47c35738040fcc3aa2ddd3a
kira-1997 <83617330+kira-1997@users.noreply.github.com>
2023-11-23 15:14:39 +0300

    Update App.jsx

--- W7-D5-ProjectTuaiq/src/App.jsx
+++ W7-D5-ProjectTuaiq/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
     <NabBar></NabBar>
+        <Underheader/> 
      <div className="cards">     
        <Card  name="برنامج" title="اختبار الاختراق للمبتدئين" locImg="https://tuwaiq.edu.sa/icons/location_icon.svg" loc="الرياض" calImg="https://tuwaiq.edu.sa/icons/caleander_icon.svg" cal="أسبوع واحد" startImg="https://tuwaiq.edu.sa/icons/caleander2_icon.svg" start=" 10\11\20123 يبداء في "  />
        <Card  name="معسكر" title="اختبار الاختراق للمبتدئين" locImg="https://tuwaiq.edu.sa/icons/location_icon.svg" loc="الرياض" calImg="https://tuwaiq.edu.sa/icons/caleander_icon.svg" cal="أسبوع واحد" startImg="https://tuwaiq.edu.sa/icons/caleander2_icon.svg" start=" 10\11\20123 يبداء في " />
@@ -24,7 +25,7 @@ function App() {
        <Card  name="برنامج" title="اختبار الاختراق للمبتدئين" locImg="https://tuwaiq.edu.sa/icons/location_icon.svg" loc="الرياض" calImg="https://tuwaiq.edu.sa/icons/caleander_icon.svg" cal="أسبوع واحد" startImg="https://tuwaiq.edu.sa/icons/caleander2_icon.svg" start=" 10\11\20123 يبداء في " />
      </div>
      <Reg></Reg>
-    <Underheader/>  
+   
       <Foot/>
 
     </>
